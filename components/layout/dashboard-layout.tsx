994303fcc857adc97730e23269eadefabcd19532
jhonroyilao <jhonroyilao@gmail.com>
2025-06-19 02:44:45 +0800

    fix: low stock condition to (>10)

--- components/layout/dashboard-layout.tsx
+++ components/layout/dashboard-layout.tsx
@@ -334,15 +334,15 @@ export default function DashboardLayout({ children, userRole }: DashboardLayoutP
             <div className="text-sm text-gray-500">&copy; 2025 BSCS 2-4. All rights reserved.</div>
             <div className="mt-4 md:mt-0 text-sm text-gray-500">
               <Link href="#" className="hover:text-ccis-blue">
-                Help
+                Jayr
               </Link>
               <span className="mx-2">•</span>
               <Link href="#" className="hover:text-ccis-blue">
-                Privacy
+                Eric
               </Link>
               <span className="mx-2">•</span>
               <Link href="#" className="hover:text-ccis-blue">
-                Terms
+                Luis
               </Link>
             </div>
           </div>
